4b79dd9a1237963285f13aa0a9e49d1ae807e3af
ivaninbox <ix.pukhov@gmail.com>
2024-04-12 14:09:10 +0500

    d

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const productController = require('../controllers/productController');
+const productController = require('../controllers/ProductController');
 const router = express.Router();
 
 router.post('/', productController.createProduct);
