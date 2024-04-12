654d16c581e8fcd55ed3223c6cbd0da25bf5c673
ivaninbox <ix.pukhov@gmail.com>
2024-04-12 14:09:59 +0500

    d

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const categoryController = require('../controllers/categoryController');
+const categoryController = require('../controllers/CategoryController');
 const router = express.Router();
 
 router.post('/', categoryController.createCategory);
